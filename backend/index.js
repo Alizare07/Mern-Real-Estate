031391c5682b5cc4aec2d7788c25541c9a69e427
Alizare07 <140269961+Alizare07@users.noreply.github.com>
2023-09-29 19:43:59 +0330

    connect to database

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,15 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import { error } from 'console'
+dotenv.config()
+
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+    console.log("connect to database")
+}).catch((error)=>{
+    console.log(error)
+})
 
 
 const app = express()

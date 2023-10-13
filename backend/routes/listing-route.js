c3880744f7f956ff2bd0d90bcc39dca44d52bec7
Alizare07 <140269961+Alizare07@users.noreply.github.com>
2023-10-13 20:32:09 +0330

    create update listing api route

--- backend/routes/listing-route.js
+++ backend/routes/listing-route.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import { verfiyToken } from '../utils/verfiyUser.js'
-import { createListing, deleteListing } from '../controllers/list-controller.js'
+import { createListing, deleteListing, updateListing } from '../controllers/list-controller.js'
 
 const router  = express.Router()
 
 
 router.post('/create', verfiyToken, createListing)
 router.delete('/delete/:id', verfiyToken, deleteListing)
+router.post('/update/:id', verfiyToken, updateListing)
 
 export default router
